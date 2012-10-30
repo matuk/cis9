30fa5b53219a9ca04282e78c2eea16d183a02455
MKaempf <makampf@gmail.com>
2012-10-30 22:17:54 +0100

    New root

--- config/routes.rb
+++ config/routes.rb
@@ -54,6 +54,7 @@ Cis9::Application.routes.draw do
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
   # root :to => 'welcome#index'
+  root :to => 'patients#index'
 
   # See how all your routes lay out with "rake routes"
 
